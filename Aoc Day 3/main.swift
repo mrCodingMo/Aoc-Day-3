066b9bca33f7a78d841ccd5ec71233218115efea
Moritz Diedenhofen <moritzdiedenhofen@googlemail.com>
2021-12-12 14:22:38 +0100

    solved

--- Aoc Day 3/main.swift
+++ Aoc Day 3/main.swift
@@ -10,33 +10,88 @@ func loadData() -> String{
 
 let content = loadData()
 var reportData = content.split(separator: "\n")
-
 var data = [[Character]]()
+var index = 0
 
-for row in reportData {
-    let arr = Array(row)
-    data.append(arr)
+let oxygenRating = getRating(typ: ratingType.oxygen)
+let scrubberRating = getRating(typ: ratingType.scrubber)
+
+let lifeSupportRating = oxygenRating * scrubberRating
+print(lifeSupportRating)
+
+func getRating(typ: ratingType) -> Int {
+
+    index = 0
+    fillData()
     
+    while data.count != 1 {
+        let numberToKeep = searchForNumber(dat: data, searchIndex: index, typ: typ)
+        data = removeLines(keep: numberToKeep, searchIndex: index)
+        index += 1
+    }
+    
+   return GetIntegerFromBit(value: String(data[0]))
 }
 
-var gammaRate = ""
-for i in 0 ... data[0].count - 1 {
+func fillData() {
+    data.removeAll()
+    for row in reportData {
+        let arr = Array(row)
+        data.append(arr)
+    }
+}
+
+func removeLines(keep: Int, searchIndex: Int) -> [[Character]] {
+    
+    for i in stride(from: data.count - 1, to: -1, by: -1){
+        if String(data[i][searchIndex]) != String(keep) {
+            data.remove(at: i)
+        }
+    }
+    return data
+}
+
+func searchForNumber(dat: [[Character]], searchIndex: Int, typ: ratingType) -> Int {
     var clm = [Character]()
     
-    for ii in 0 ... data.count - 1 {
-        clm.append(data[ii][i])
+    for i in 0 ... dat.count - 1 {
+        clm.append(dat[i][searchIndex])
     }
     
     let zeroCount = clm.filter { $0 == "0"}.count
     let oneCount = clm.filter { $0 == "1"}.count
     
-    if zeroCount > oneCount {
-        gammaRate += "0"
-    } else if oneCount > zeroCount {
-        gammaRate += "1"
-    } else {
+    if typ == ratingType.oxygen {
+        if zeroCount > oneCount {
+            return 0
+        } else if oneCount > zeroCount {
+            return 1
+        } else {
+            return 1
+        }
+        
+        
+    } else if typ == ratingType.scrubber {
+        if zeroCount > oneCount {
+            return 1
+        } else if oneCount > zeroCount {
+            return 0
+        } else {
+            return 0
+        }
         
     }
+    return 0
 }
-print(gammaRate)
 
+func GetIntegerFromBit(value: String) -> Int {
+    if let v = Int(value, radix: 2) {
+        return v
+    }
+    return -1
+}
+
+enum ratingType {
+    case oxygen
+    case scrubber
+}
